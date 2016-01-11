bdd2ea7fb66ed610534c2e146acf506930b74525
dannyd4315 <dannyd4315@gmail.com>
2016-01-11 16:50:55 +1100

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Danny Dawson"
+  end 
+
+  get '/hometown' do 
+    "My hometown is Melbourne"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Hometown Glory"
+  end
 end
