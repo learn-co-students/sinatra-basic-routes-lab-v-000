3969e82e86d28d644d21a2aadfe70d64a8287389
Louiefigz <figueroa.aaron@gmail.com>
2016-01-11 21:40:50 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Hans Solo"
+  end 
+
+  get '/hometown' do
+    "My hometown is Unknown"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is the star wars theme music"
+  end 
 end
