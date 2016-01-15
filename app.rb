8c2593ec578bc104cd4380739e9dd18fca4bc9dd
Chris Aporta <christopher.aporta@gmail.com>
2016-01-15 00:44:10 -0500

    complete lab

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Chris"
+  end
+
+  get '/hometown' do
+    "My hometown is West Orange"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Africa by Toto"
+  end
 end
