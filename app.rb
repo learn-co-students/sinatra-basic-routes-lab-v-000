54b726d95d6cd5515e98b42e96ce5fe622eecc56
surla <josephsurla@gmail.com>
2016-01-15 22:59:31 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Joseph Surla"
+  end
+
+  get '/hometown' do
+    "My hometown is San Diego"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is To My Soul by Jerry Folk"
+  end
+
 end
