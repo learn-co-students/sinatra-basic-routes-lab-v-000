6aef533f0f61b7d94aaaff064f6980b704f3d4d9
Tracy <tracetrace@gmail.com>
2016-01-17 07:03:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+  
+  get '/name' do
+    "My name is Tracy."
+  end
+
+  get '/hometown' do
+    "My hometown is Little Rock, AR."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Modern Love"
+  end
+
 end
