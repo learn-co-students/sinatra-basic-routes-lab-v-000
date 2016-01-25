35154abb28446a2063ac46a6412193291e5d157c
c1505 <coreym247@gmail.com>
2016-01-24 19:22:02 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Corey"
+  end
+
+  get '/hometown' do 
+    "My hometown is Apex"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Crazy"
+  end
 end
