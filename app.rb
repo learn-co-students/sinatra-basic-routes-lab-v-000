550ae64bb7ba45c5e084cb2b7533f3c70db887ee
febbraiod <donedesign1@gmail.com>
2016-01-26 16:05:10 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Don"
+  end
+
+  get "/hometown" do
+    "My hometown is Norwalk"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Fly me to the Moon.'"
+  end
+
+
 end
