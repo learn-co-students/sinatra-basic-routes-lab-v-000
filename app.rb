1d2059194fbba7bea66334d560e389f366db36ec
Brett <brettrheenan@gmail.com>
2016-01-26 10:42:48 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do 
+		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Brett"
+	end
+
+	get '/hometown' do
+		"My hometown is San Rafael"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is 'Happiness is a Warm Gun"
+	end
+
+
 end
