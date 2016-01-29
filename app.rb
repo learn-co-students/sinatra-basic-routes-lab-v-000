7b62911c1906f2cbd4d0a281bc8d44d10bfcc23b
azheng249 <andy.zheng249@gmail.com>
2016-01-29 13:57:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"  
+  end
+
+  get '/name' do
+    "My name is Kristaps Porzingis."
+  end
+
+  get '/hometown' do
+    "My hometown is Liepāja, Latvia."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is WorldStar."
+  end
+
 end
