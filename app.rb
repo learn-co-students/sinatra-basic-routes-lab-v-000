49f105c526765834288517d7dae4380cafc49925
Tsundu <osmentdan@gmail.com>
2016-01-31 13:17:06 -0500

    added routes

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+ # get '/' do 
+  #  "Hello Yo"
+  #end 
+
+  get '/name' do 
+     "My name is Dan"
+  end
+
+  get '/hometown' do  
+      "My hometown is Reading"
+
+  end 
+
+  get '/favorite-song' do 
+     "My favorite song is Graduation"
+  end 
+
+
 end
