53d439e0254e2b00e76eafc79dc97f0f91b88509
empireofryan <rjohnson2001@gmail.com>
2016-02-01 00:14:03 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Ryan"
+  end
+  get '/hometown' do
+    "My hometown is Clifton, VA"
+  end
+  get '/favorite-song' do
+    "My favorite song is Love Sosa"
+  end
 end
