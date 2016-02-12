22c9c2e81e3a39442f44de8437868240606efff5
tuckerbohman5 <tucker.bohman@gmail.com>
2016-02-11 21:24:41 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do 
+    "My name is Tucker"
+  end
+
+  get '/hometown' do
+    "My hometown is Salt Lake City"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Slide by The Goo Goo Dolls"
+  end
 end
