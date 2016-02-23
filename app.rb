373f81e96067aa5ad07e322590b7376a0e15bfb6
Smulligan85 <sean.mulligan85@gmail.com>
2016-02-23 07:57:56 -0800

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Sean"
+  end
+
+  get "/hometown" do
+    "My hometown is Mars"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Lung"
+  end
+
 end
