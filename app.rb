c8e8bbe9f66fcff1906e96c7676181ec637c8a82
James Novak <jamesnovak90@gmail.com>
2016-02-27 19:45:55 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is James"
+  end
+
+  get '/hometown' do 
+    "My hometown is Old Bridge"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Never Gonna Give You Up"
+  end
 end
