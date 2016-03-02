0ced7a84ee66fc72d35b1f8e17cdd0cf2877298e
fiveinfinity <jlachance1@gmail.com>
2016-03-02 12:52:42 -0600

    rspec passing

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Joe"
+  end
+
+  get '/hometown' do
+    "My hometown is Hastings, NE"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bittersweet Symphony"
+  end
 end
