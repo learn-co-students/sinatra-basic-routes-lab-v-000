81a55673cebae213ae403933f9ccad502c56249f
ed-karabinus <ejkarabin@gmail.com>
2016-03-04 08:29:57 -0600

    Finished Sinatra Basic Routes Lab

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Ed."
+  end
+
+  get '/hometown' do
+    "My hometown is Hudson, OH."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is \"It Could Happen To You\" by Jimmy Van Heusen and Johnny Burke."
+  end
 end
