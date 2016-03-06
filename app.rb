f4182ec7c364e3bf1d52e149dc270305b251a427
jjlippman <jjlippman@earthlink.net>
2016-03-06 10:09:49 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  def '/name 'do 
+    "My name is Jim."
+
+  end
+
+  def '/hometown 'do 
+    "My hometown is St. Louis"
+
+  end
+
+
+
+  def '/favorite-song 'do 
+    "My favorite-song is IDK"
+
+  end
+
 end
