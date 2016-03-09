ab9bf36ac605fb4962cafeedd1a95b28ddd1fac1
thelucasmoore <lucasedwardmoore@gmail.com>
2016-03-09 15:10:17 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		"Hello, World!"	
+	end
+
+	get '/name' do
+		"My name is Lucas."
+	end
+
+	get '/hometown' do
+		"My hometown is Planet Earth."
+	end
+
+	get '/favorite-song' do
+		"My favorite song is ANY Funky Music."
+	end
 end
