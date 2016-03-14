c3b7e90c362f9f0f807b125c5321386da7fa8a85
Ziv Zamechek <ziv921@yahoo.com>
2016-03-14 12:46:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "Hello, World! My name is Ziv"
+  end
+
+  get '/hometown' do
+    "My hometown is Netanya"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is See You Again"
+  end
+
 end
