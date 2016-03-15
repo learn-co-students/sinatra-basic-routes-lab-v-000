b5653c1900468f600992ec7a7783ee7ee74d39c1
codeesh <vineshpersaud@gmail.com>
2016-03-15 15:45:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+ get '/' do
+      "Hello, World!"
+ end
+
+ get ('/name') {"My name is Vinesh"}
+ 
+ get('/hometown'){"My hometown is Queens NY"}
+
+ get('/favorite-song'){"My favorite song is Juicy"}
+ 
+
+
+
 end
