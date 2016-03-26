1d6c37f6e15ffc572c7ea9c409dabdc0b230c0dd
seanliu93 <sean93liu@gmail.com>
2016-03-25 23:58:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    resp = Rack::Response.new
+    resp.write "My name is Sean"
+    resp.status = 200
+    resp.finish
+  end
+
+  get '/hometown' do
+    resp = Rack::Response.new
+    resp.write "My hometown is Novi, Michigan"
+    resp.status = 200
+    resp.finish
+  end
+  get '/favorite-song' do
+    resp = Rack::Response.new
+    resp.write "My favorite song is Lose Yourself to Dance - Daft Punk"
+    resp.status = 200
+    resp.finish
+  end
 end
