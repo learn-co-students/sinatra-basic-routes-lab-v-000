38b356f523f2177ad99e0edae2a78fa5a945f07e
jmheyd <heydjm@comcast.net>
2016-03-28 16:12:40 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do 
+  	"My name is Julie Heyd"	
+  end
+
+  get '/hometown' do
+  	"My hometown is Falcon Heights, MN"
+  end
+
+  get '/favorite-song' do
+  	"My favorite song is 'Don't Give Up on Me'"
+  end
 end
