9cd09a41b316b1555282520baf82c720ff46409a
bedwardsphoto <brian@brianedwardsphotography.com>
2016-03-31 13:01:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Brian"
+  end
+
+  get '/hometown' do
+    "My hometown is Montrose, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Smells Like Teen Spirit"
+  end
 end
