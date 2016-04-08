f77ab58c866db891c136d66d9fcca5acb0f6da96
mirandalily <mirandaraymond@me.com>
2016-04-08 11:30:41 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Miranda."
+  end
+
+  get '/hometown' do
+    "My hometown is Seattle."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Heart of Life."
+  end
+
 end
