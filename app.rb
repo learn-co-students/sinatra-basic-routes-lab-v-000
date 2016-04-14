560e6c4252b1c7d6df9f432380752d1125d7f5bf
katherinethai <kthai1618@gmail.com>
2016-04-14 13:14:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Katherine"
+  end
+
+  get '/hometown' do
+    "My hometown is Voorhees"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Semi-Automatic"
+  end
 end
