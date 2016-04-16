35adbe72d31b1f2bf6555316767ad19729a4bd99
Nathaniel Miller <dukeoflaser@gmail.com>
2016-04-16 17:15:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Nathaniel."
+  end
+  
+  get '/hometown' do
+    "My hometown is Vancouver, BC."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is M83's 'Outro'."
+  end  
 end
