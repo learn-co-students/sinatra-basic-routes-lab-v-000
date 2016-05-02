6e54a176bef8aabb27b1ffe011d7ba855bbadee7
wsaxe <geosaxe@gmail.com>
2016-05-01 20:12:44 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is William Saxe."
+  end
+
+  get '/hometown' do
+    "My hometown is Richfield Springs, NY."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Beyond the Sea'."
+  end
+
 end
