c726330ef81d456efab2048ed34fd9034970efdc
marcelgalang <marcelgalang@gmail.com>
2016-05-04 14:07:42 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Marcel"
+  end
+
+  get '/hometown' do
+    "My hometown is Milwaukee."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Suspicious Minds'"
+  end
+
+
 end
