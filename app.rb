4f2b082e4711b6a34bfc0750f25a4908f5676dc6
danielriley06 <driley3@gmail.com>
2016-05-14 15:49:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Dan Riley"
+  end
+  get '/hometown' do
+    "My hometown is Memphis, TN"
+  end
+  get '/favorite-song' do
+    "My favorite song is Subterranean Homesick Alien"
+  end
 end
