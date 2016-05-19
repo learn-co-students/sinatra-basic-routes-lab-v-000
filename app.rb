c7cc0826bd4a11138faef6e4e0af2a1d5c89bbea
akizzles <aykim002@gmail.com>
2016-05-19 03:54:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Andy"
+  end
+
+  get '/hometown' do
+    "My hometown is Owensboro"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Kid A"
+  end
 end
+
+
+
+
+# get '/medicines' do
+#   @medicines = Medicine.all
+ 
+#   erb :'medicines/index.html.erb'
+# end
