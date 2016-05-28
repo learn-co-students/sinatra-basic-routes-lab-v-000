4497d167b87847f458c27507e0d690db2e000e7e
karuna24s <karuna24s@gmail.com>
2016-05-28 13:28:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Karuna."
+  end
+
+  get '/hometown' do
+    "My hometown is Forest Hills, NY."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Counting Stars by One Republic."
+  end
+
 end
