121cdd532ca0e8b2104da23f2d9b788294d1073b
kade042 <dereje88@gmail.com>
2016-05-30 07:44:52 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  resp = Rack::Response.new
+  get('/name') { resp.write "My name is "}
+  get('/hometown') {resp.write "My hometown is "}
+  get('/favorite-song') {resp.write "My favorite song is "}
 end
