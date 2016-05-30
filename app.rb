640a90b66c6fd6bc78127889371b15d35a8340fb
kade042 <dereje88@gmail.com>
2016-05-30 07:49:04 -0600

    Done.

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ class App < Sinatra::Base
 
   resp = Rack::Response.new
 
-  get('/name') { resp.write "My name is "}
-  get('/hometown') {resp.write "My hometown is "}
-  get('/favorite-song') {resp.write "My favorite song is "}
+  get('/name') { resp.write "My name is Dereje"}
+  get('/hometown') {resp.write "My hometown is Addis Ababa"}
+  get('/favorite-song') {resp.write "My favorite song is Three Little Birds"}
 
 end
