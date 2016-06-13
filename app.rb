7aabc498c3a2e3f118097640ac16325309d133da
Savannah Scott <sascott624@gmail.com>
2016-06-13 20:31:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+      "My name is Savannah"
+    end
+
+    get '/hometown' do
+      "My hometown is Irvington"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is 'Weathered' by Jack Garratt"
+    end
+
 end
