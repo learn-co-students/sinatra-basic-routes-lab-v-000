f394287e6d3c3b9e32433006f8c749a6f3cd78e2
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-06-16 12:58:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Michael"
+  end
+
+  get '/hometown' do
+    "My hometown is Wellington"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is One Mic"
+  end
 end
