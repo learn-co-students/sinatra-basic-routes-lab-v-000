579a11e5bd6218403da77bf7d40de9c8126360ab
Jason Casares <jasonc@csdrafting.com>
2016-06-22 01:11:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Jason"
+  end
+
+  get '/hometown' do
+    "My hometown is Sherman, TX"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Dance me to the end of Love"
+  end
+
+  
 end
