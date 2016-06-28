96c2b936dfb8c56726fd22daa0f32b519becd523
Joseph Lim <joseph.d.lim@gmail.com>
2016-06-28 13:26:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+
+    "My name is Joseph."
+
+  end
+
+  get '/hometown' do
+
+    "My hometown is New York"
+
+  end
+
+  get '/favorite-song' do
+
+    "My favorite song is Old MacDonald"
+
+  end
 end
