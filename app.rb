d60d416794d22cd11e250af02a3f398efe3fc44c
andrewstone8 <andrewstone8@gmail.com>
2016-07-01 14:51:09 +0900

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Andrew"
+  end
+
+  get '/hometown' do
+    "My hometown is Lexington"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bach Cello Suite No. 1 in G"
+  end
 end
