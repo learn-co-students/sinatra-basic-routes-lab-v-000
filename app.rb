64c2680978329cdf86091bf26c5ce4521ad84b49
Sandy Hogan <svern99@aol.com>
2016-07-04 00:34:59 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Sandy"
+  end
+
+  get '/hometown' do
+    "My hometown is Marshfield"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is probably a Garth song"
+  end
+
 end
