c68c15c7f78250bb9cf07db1e9de56e6a9573b2f
kromoser <kevinromoser@gmail.com>
2016-07-04 11:35:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Kevin." 
+  end
+
+  get "/hometown" do
+    "My hometown is Brooklyn, NY."
+  end
+
+  get "/favorite-song" do
+    "My favorite song is La Le Lo."
+  end
 end
