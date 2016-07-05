725a91fdaba2bcbc2219eeb133204f4947676a51
ebanh <ebanhernandez@gmail.com>
2016-07-05 16:55:15 +0900

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Eban"
+  end
+
+  get '/hometown' do
+    "My hometown is Seattle"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Holocene"
+  end
 end
