7f83087bcfaa33fa65a7782d78a4582d543980ea
Han Lee <hanlee12@gmail.com>
2016-07-11 20:51:26 +0000

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Han Solo"
+  end
+
+  get '/hometown' do
+    "My hometown is Chicago"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Say you will'"
+  end
+
 end
