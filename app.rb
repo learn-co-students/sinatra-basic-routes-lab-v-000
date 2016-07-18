d4ffe8794c2024c9dc75479a443b7719a6ab97aa
Adam Taitano <adam.taitano@gmail.com>
2016-07-18 22:00:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Adam"
+  end
+
+  get '/hometown' do
+    "My hometown is Portland"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'I Felt Your Shape'"
+  end
 end
