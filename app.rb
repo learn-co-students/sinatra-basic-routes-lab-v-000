6bb1cd9a99dda87c3afb8c5844a409601b56eacd
Caleb Faught <faughtcj@gmail.com>
2016-07-27 16:21:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Caleb Faught."
+  end
+
+  get '/hometown' do
+    "My hometown is Denton, TX."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Magic Carpet Ride"
+  end
 end
