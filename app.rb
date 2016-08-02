bd5657baf42873b44175b257f386bf47434d6b94
auranbuckles <auran59@gmail.com>
2016-08-02 11:06:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do
+		"My name is Auran."
+	end
+
+	get '/hometown' do
+		"My hometown is Hong Kong."
+	end
+
+	get '/favorite-song' do
+		"My favorite song is PONPONPON."
+	end
+
 end
