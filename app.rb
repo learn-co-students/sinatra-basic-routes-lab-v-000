0d845961378e0678d9c35d8c62804dad61ed9796
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-21 17:05:39 +0000

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get  '/name' do
+     "My name is __"
+    end
+    status = 200
+
+    get  '/hometown' do
+     "My hometown is __"
+    end
+    status = 200
+
+    get  '/favorite-song' do
+     "My favorite song is __"
+    end
+    status = 200
+
 end
