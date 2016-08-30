34397b2aa793eafbf92eb2c5ca836e866917f348
Ben Norris <bennorrismisc@gmail.com>
2016-08-30 14:14:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World."
+  end
+
+  get '/name' do
+    "My name is Pentti."
+  end
+
+  get "/hometown" do
+    "My hometown is Helsinki, Finland."
+  end
+
+  get "/favorite-song" do
+    "My favorite song is This I Dig of You."
+  end
+
+
 end
