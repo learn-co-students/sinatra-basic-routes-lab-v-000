f4dd46008bcbbb736dbd12777ee8961a3f68e1ba
William Jones <enquiries@forestfed.com>
2016-08-30 00:10:46 +0000

    learn sinatra basic routes

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Bill Jones"
+  end
+  get '/hometown' do
+    "My hometown is Dillwyn"
+  end
+  get '/favorite-song' do
+    "My favorite song is 1 in 10"
+  end
 end
