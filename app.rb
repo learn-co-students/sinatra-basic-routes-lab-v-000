df3c1ee4bab19f5fa5df3b2d32b9387b39babfb0
SnowboardTechie <bryan@snowboardtechie.com>
2016-09-03 21:31:10 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Bryan"
+  end
+
+  get '/hometown' do
+    "My hometown is Huntington Beach"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Chaccaron"
+  end
 end
