20ceb72e9db961e0a4efecd9b7d65316cc0b281c
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-07 18:49:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    status 200
+    "My name is Kathleen"
+  end
+
+  get '/hometown' do
+    status 200
+    "My hometown is Exeter"
+  end
+
+  get '/favorite-song' do
+    status 200
+    "My favorite song is 'I'm Just a Girl' by No Doubt"
+  end
+
 end
