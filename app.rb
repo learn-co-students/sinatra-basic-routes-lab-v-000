0510b0a19503818b70f7db4bd82129d8ed8a668f
BriOD <briodonoghue@gmail.com>
2016-09-12 17:49:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+end
+
+  get '/name' do
+    "My name is Brian"
+  end
+
+  get '/hometown' do
+    "My hometown is Floral Park"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is With A Little Help From My Friends"
+  end
 end
