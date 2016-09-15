8f0e0d54a1cd989bccc4a82d17442ebbd0fc4b62
Bob Habig <rfhab2@gmail.com>
2016-09-15 21:11:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Spartacus"
+  end
+
+  get '/hometown' do
+    "My hometown is SPARTA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Battle Cry"
+  end
 end
