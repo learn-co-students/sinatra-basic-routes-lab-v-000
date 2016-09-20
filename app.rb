be22d419f527ec54c30f2bebaf212b457027ceb6
Lani1321 <lani.berry13@gmail.com>
2016-09-20 10:16:14 -0400

    build name, hometown and favorite-song routes

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Lani"
+  end
+
+  get '/hometown' do 
+    "My hometown is Honolulu, Hawaii"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is Closer"
+  end
+
 end
