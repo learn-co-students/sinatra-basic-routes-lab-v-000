7d3e3bd91698dbad71f31444e85b58ec9b5a5649
AliceKB <alicekristinabalbuena@gmail.com>
2016-09-26 14:20:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Alice"
+  end
+
+  get '/hometown' do
+    "My hometown is clifton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is What a Catch, Donnie"
+  end
 end
