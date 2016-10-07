2d124d38b1145727e60c397ca466b16733fe2ea6
David Kennell <dkennell998@hotmail.com>
2016-10-07 04:10:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+        "My name is David"
+  end
+
+  get '/hometown' do
+      "My hometown is Tallahassee"
+    end
+
+  get '/favorite-song' do
+      "My favorite song is My Dingaling by Chuck Berry"
+    end
+
+  end
