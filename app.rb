6b77f8ab319104a70f2d6109ef3654a3cfe0c739
Aya Fushimi <aya.fushimi@gmail.com>
2016-11-02 23:47:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Aya."
+  end
+
+  get "/hometown" do
+    "My hometown is Tokyo."
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Muerte del Angel' by Astor Piazzolla."
+  end
+
 end
