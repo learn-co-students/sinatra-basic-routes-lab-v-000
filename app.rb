1d2e9647845a0744485ec4c8f56e059e31927231
Elaine Miller <emiller7149@gmail.com>
2016-11-12 21:27:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Elaine"
+  end
+
+  get '/hometown' do
+    "My hometown is Fairfield"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Ain't No Stopping Sunshine"
+  end
+
 end
