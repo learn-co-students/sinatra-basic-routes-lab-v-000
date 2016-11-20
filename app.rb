9a8d4293850e9efd74f65f19f23486e302f71925
Andres Duarte <andresduarte010@gmail.com>
2016-11-20 19:35:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/hometown' do
+    "My hometown is Bogota"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Rome"
+  end
+
+  get '/name' do
+    "My name is Andres"
+  end 
 end
