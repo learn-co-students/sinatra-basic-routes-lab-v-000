75da4ec0697b5988f254a2cfdbaa88f184440bcd
Malind Nikulski <malind.nikulski@gmail.com>
2016-11-21 18:39:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Malind Evalette Kowalski"
+  end
+
+  get '/hometown' do
+    "My hometown is Long Beach, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 80's Mercedes right now"
+  end
+
 end
