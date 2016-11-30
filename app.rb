0c3f5ab1175ab1dd2aa7506bb029aa1e7cad8309
Christina Cole <christinal.cole@gmail.com>
2016-11-30 19:44:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Elmer."
+  end
+
+  get '/hometown' do
+    "My hometown is the Warner Bros.' lot."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'We're hunting wabbit.''"
+  end
+
 end
