48a90d99d108251b8d1fbac691e9bdf9f16148c5
mjdele <mike.delehant@gmail.com>
2016-12-08 20:13:00 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mike."
+  end
+
+  get '/hometown' do
+    "My hometown is Sacramento, California."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Nancy From Now On."
+  end
 end
