4f6c96e551f77e209dd0363409a6ad394cd41db5
Adam Lucia <lucia.adam@gmail.com>
2016-12-29 05:27:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Adam."
+  end
+
+  get '/hometown' do
+    "My hometown is Pittsburgh."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bang Bang."
+  end
 end
