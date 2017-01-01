0ed8f0e290cc49b2a06717c87fc86d2073323c38
Logan McAnsh <logan@mcan.sh>
2017-01-01 18:32:40 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Logan"
+  end
+
+  get '/hometown' do
+    "My hometown is Shelby Township"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Jam by Logic"
+  end
+
 end
