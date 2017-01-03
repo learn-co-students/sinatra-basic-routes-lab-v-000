f9b01948a84bfb199f3579c7d6c3545b59dcb8cd
Mary Umoh <umohm12@gmail.com>
2017-01-03 16:52:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+  class App < Sinatra::Base
+
+    get '/name' do
+      "My name is Mary."
+  end
+
+   get '/hometown' do
+    "My hometown is Trenton"
+  end
+
+   get '/favorite-song' do
+      "My favorite song is By Your Side"
+   end 
+
 end
