85c2a335a3906a21cfee9dba669ae15c3954d3d6
Matthew Hallquist <matthallquist@yahoo.com>
2017-01-04 17:44:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Matt."
+  end
+
+  get '/hometown' do
+    "My hometown is East Hartford, CT."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Friend of the Devil."
+  end
 end
