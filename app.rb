3a38d12fe61c4c8168c3b73cc47088c41fbda1a2
warrend <warrend555@gmail.com>
2017-01-18 00:37:16 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Daniel"
+  end
+
+  get '/hometown' do
+    "My hometown is Columbus, Ohio"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is I've Got a Feeling by The Beatles"
+  end
+
 end
