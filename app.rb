9a81bd810754c3ece165b787184e08f062b482ce
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2017-02-04 23:08:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Idayat Oshobajo"
+  end
+  get '/hometown' do
+    "My hometown is Brooklyn"
+  end
+  get '/favorite-song' do
+    "My favorite song is Promine  by Jagged Edge "
+  end
+
+
 end
