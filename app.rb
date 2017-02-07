313440a08b3f2a12b81bc66a5eca8fdff403ad55
vanita eckert <vanita112003@gmail.com>
2017-02-07 02:46:47 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Vanita"
+  end
+
+  get("/hometown"){"My hometown is Hougang"}
+
+  get("/favorite-song") do
+    "My favorite song is 23 by Jimmy Eat World"
+  end
+  
 end
