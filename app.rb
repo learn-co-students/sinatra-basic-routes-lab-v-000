af555dd2e56b6fadfbb86587f57578c0bf0bf4ca
M. Simon Borg <msimonborg@gmail.com>
2017-02-13 23:20:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    'My name is Cersei Lannister'
+  end
+
+  get '/hometown' do
+    'My hometown is Casterly Rock'
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'The Rains of Castamere'"
+  end
 end
