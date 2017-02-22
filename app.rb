88de60128f5ebe489481137da6070340bcceaab9
vicision <duncan.j.mclachlan@gmail.com>
2017-02-22 17:43:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Duncan"
+  end
+
+  get '/hometown' do
+    "My hometown is Walton, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is different every day"
+  end
+  
 end
