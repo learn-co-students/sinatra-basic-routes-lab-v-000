7c58c81a866354bf15699ab764f250f78b20ae9c
Benny Hudson Hung <wakenightryu@gmail.com>
2017-03-06 23:58:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Benny"
+  end
+
+  get '/hometown' do
+    "My hometown is New York"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Always On My Mind"
+  end
 end
