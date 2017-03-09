6acd7eb421227a0d929ce6fe554ecc68ed734a8a
Hui Jiang <hui1021@yahoo.com>
2017-03-09 17:53:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/name' do
+    "My name is Hannah"
+  end
+  
+  get '/hometown' do
+    "My hometown is NY, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is School Bus Go Round and Round"
+  end
+
 end
