5e08b6ebe987342aabc87b02dd2ab84a71849938
Kelsey Huse <kelseyhuse30@gmail.com>
2017-03-24 11:54:31 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do
+		"My name is Kelsey"
+	end
+
+	get '/hometown' do
+		"My hometown is Enid, OK"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Passionfruit"
+	end
+	
 end
