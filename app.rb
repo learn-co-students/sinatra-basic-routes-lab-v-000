d49fd0eb8120dbcbdaa3a0a8f08dc47f96cc0b43
Allison Chesky <afc340@nyu.edu>
2017-04-07 16:25:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Allison"
+  end
+
+  get '/hometown' do
+    "My hometown is Holyoke"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Love You Like That"
+  end
 end
