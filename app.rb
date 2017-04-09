7a492f37a79b0cb4ac5d88d540556295e63983aa
Erika Hughes <ehughes@knights.ucf.edu>
2017-04-09 01:24:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Erika"
+  end
+
+  get '/hometown' do
+    "My hometown is Orlando, Florida"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is currently Guru by Coast Modern"
+  end
 end
