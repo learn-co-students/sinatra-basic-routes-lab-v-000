a27f965ce41f1e09e0ad50e0d6340452f5d20a50
Annette  Drapalski <annette.drapalski@gmail.com>
2017-04-09 04:15:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Annette"
+  end
+
+  get '/hometown' do
+    "My hometown is Renwick"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Macarthur Park"
+  end
+
 end
