0f16e7b9930f68c346d34d5767b5e286e2489c7f
Trai Compton <tlcompton2546@outlook.com>
2017-04-10 18:24:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   get '/' do
+     "Hello World !!"
+   end
+
+   get '/name' do
+     "My name is Trai Lynne Compton"
+   end
+
+   get '/hometown' do
+     "My hometown is Denver, CO"
+   end
+
+   get '/favorite-song' do
+     "My favorite song is 'It Won\'t Stop' x Sevyn Streeter & Chris Brown"
+   end
 end
