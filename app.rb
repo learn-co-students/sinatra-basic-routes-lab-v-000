13eaa6a55451c62fc6b243d134af43eabfe99b70
Scott Atkins <satkins16@gmail.com>
2017-04-12 13:00:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name"  do
+    "My name is Scott Atkins"
+  end
+
+  get "/hometown" do
+    "My hometown is Short Hills"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Can't Find My Way Home"
+  end
+
 end
