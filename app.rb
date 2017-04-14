2de084f1211a230119de3e0951ba253b5a881ed0
Alex Pepper <axpepper@gmail.com>
2017-04-13 18:56:47 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Alex Pepper"
+  end
+  get '/hometown' do
+    "My hometown is Delray Beach"
+  end
+  get '/favorite-song' do
+    "My favorite song is In the Aeroplane Over the Sea"
+  end
 end
