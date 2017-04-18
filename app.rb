8059c83a3367b83ef9267423f0ba33515a05ebb4
Parv Kumar <pkumar@trinity.edu>
2017-04-18 16:05:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Parv."
+  end
+  get "/hometown" do
+    "My hometown is Brookfield, Wisconsin."
+  end
+  get "/favorite-song" do
+    "My favorite song is Gooey by Glass Animals."
+  end
+
 end
