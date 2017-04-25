25d8dacd9068c70b5cfff2b64d81a624c25c4347
ETphonehomeslice <mk.etlinger@gmail.com>
2017-04-25 16:25:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    status 200
+    "My name is Mikie"
+  end
+
+  get '/hometown' do 
+    status 200
+    "My hometown is Trenton, MI"
+  end
+
+  get '/favorite-song' do 
+    status 200
+    "My favorite song is Escape Song(Pina Colada)"
+  end
+
 end
