69d2f3c72ef4198e18cf065f7e38222746bdda06
Feiran Wang <wangfeiranpku@163.com>
2017-05-02 12:57:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Feiran"
+  end
+
+  get "/hometown" do
+    "My hometown is Tianjin"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is idk"
+  end 
 end
