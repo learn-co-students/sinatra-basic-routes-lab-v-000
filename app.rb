35577d8117df5d8d0899955ba7336cf33509128a
lucketta <lucketta@umich.edu>
2017-05-04 16:20:41 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, there"
+  end
+
+  get '/name' do
+    "My name is Areanna"
+  end
+
+  get '/hometown' do
+    "My hometown is IDK"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is who cares?"
+  end
 end
