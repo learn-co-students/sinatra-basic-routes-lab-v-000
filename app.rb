19dba480220dccb540055c88a38a3abdc3400073
David Mars <david.w.mars@my.occc.edu>
2017-05-05 05:47:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+ get '/name' do
+   "My name is Havick"
+ end
+
+ get '/hometown' do
+   "My hometown is Goldsby Oklahoma"
+ end
+
+  get '/favorite-song' do
+    "My favorite song is Back From Cali"
+  end
+ end
