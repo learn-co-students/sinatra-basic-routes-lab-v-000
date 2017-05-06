fcc38c37bee06371873e3048d8ab978f215f847a
Laamia Islam <laamia.islam@gmail.com>
2017-05-06 15:20:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Laamia."
+  end
+
+  get '/hometown' do
+    "My hometown is New Orleans."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is anything by Justin Bieber."
+  end
+
 end
