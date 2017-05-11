f6e8ccf6fafcd6308575660567aaee59fea39c53
r0tiart <antley.chu@gmail.com>
2017-05-11 13:41:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		"Hello World!"
+	end
+
+	get '/name' do
+		"My name is Antley"
+	end
+
+	get '/hometown' do 
+		"My hometown is New York City"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Lose Yourself"
+	end
 end
