eff3a66440ffa65b3a8fbeb6c00feb9e7d2bc5ba
Aditi Green <asaki444@gmail.com>
2017-05-13 22:19:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World"
+  end
+  get '/name' do
+     "My name is Aditi"
+  end
+  get '/hometown' do
+    "My hometown is NYC"
+  end
+  get '/favorite-song' do
+    "My favorite song is Bennie and Jets"
+  end
 end
