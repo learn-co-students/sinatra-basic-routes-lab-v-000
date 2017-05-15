936542b757bcdd6dff87e6b8206ec4ae82c8279b
Sara Hopper <sara.hopper@gmail.com>
2017-05-15 17:53:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+
+    get '/name' do
+      "My name is Sara"
+    end
+
+    get '/hometown' do
+      "My hometown is Brielle"
+    end
+
+    get '/favorite-song' do
+      "My favorite song is Life On Mars"
+    end
+  end
