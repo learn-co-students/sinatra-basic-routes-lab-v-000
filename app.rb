d2302cb7edaeed665412520b6619972dd43e122c
WilliamBarela <william.barela.dev@gmail.com>
2017-05-16 18:39:36 +0900

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is William"
+  end
+
+  get '/hometown' do
+    "My hometown is Las Cruces"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is currently Manastra by Summer Salt"
+  end
 end
