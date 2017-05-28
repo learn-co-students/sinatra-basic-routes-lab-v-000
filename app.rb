6e2b4ae72b43a8eef966f6204d04a193bd6526ba
Graham Squires <graham.squires3@gmail.com>
2017-05-28 21:06:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Graham"
+end
+
+get '/hometown' do
+  "My hometown is Caldwell, NJ"
+end
+
+get '/favorite-song' do
+  "My favorite song is Paint it black"
+end
 end
