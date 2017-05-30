8f587dee58e23ca87517bfb9fdf8d0167a544214
Saad <Dropheaven@live.com>
2017-05-30 12:52:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get ('/name') { "My name is Saad Ahmed" }
+  get ('/hometown') { "My hometown is Brooklyn." }
+  get ('/favorite-song') { "My favorite song is Experience by Ludovico Einaudi" }
 end
