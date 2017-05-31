23f84e9c3d63063fa2aa6da33814b39bdad2ee4a
Joseph Hyatt <jhyatt1017@gmail.com>
2017-05-31 02:10:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
+#####Basic Routes Lab
+#####GET '/name'
+#returns a 200 status code
+#displays your name
+#####GET '/hometown'
+#returns a 200 status code
+#displays your hometown
+#####GET '/favorite-song'
+#returns a 200 status code
+#displays your favorite song
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is __"
+  end
+
+  get '/hometown' do
+    "My hometown is __"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is __"
+  end
 end
