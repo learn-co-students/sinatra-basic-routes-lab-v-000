c5692bd00179b06ae3d88b25b7f961be0c9cd5c2
Joseph Hyatt <jhyatt1017@gmail.com>
2017-05-31 02:15:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,15 +13,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/name' do
-    "My name is __"
-  end
-
-  get '/hometown' do
-    "My hometown is __"
-  end
-
-  get '/favorite-song' do
-    "My favorite song is __"
-  end
+  get ('/name') { "My name is Joseph Hyatt" }
+  get ('/hometown') { "My hometown is Victorville" }
+  get ('/favorite-song') { "My favorite song is Hero of the Day by Metallica" }
 end
