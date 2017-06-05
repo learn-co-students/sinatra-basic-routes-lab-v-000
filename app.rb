9424e5565252dabe92884d0ab5e6a45627df15f6
Samantha Knight <sammay82@yahoo.com>
2017-06-05 19:01:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Samantha."
+end
+
+get '/hometown' do
+"My hometown is Lawrenceburg,KY."
+end
+
+get '/favorite-song' do
+"My favorite song is The Wheels on the Bus."
+end
 end
