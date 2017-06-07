e4de6b10f68461662b5af802d446bc814fc845f1
Sam <sam.zeidman@gmail.com>
2017-06-07 13:42:35 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
+  get '/name' do
+    "My name is what"
+  end
+
+  get '/hometown' do
+    "My hometown is what"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is what"
+  end
+
 class App < Sinatra::Base
 end
