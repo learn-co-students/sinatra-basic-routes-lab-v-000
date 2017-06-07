3dcc1d89a3d6a9a835c5fb214d94c0a89fa3959b
Dave Schlauderaff <dschlaud@gmail.com>
2017-06-07 13:10:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Dave Schlauderaff"
+  end
+
+  get "/hometown" do
+    "My hometown is New Orleans"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Starlight"
+  end
 end
