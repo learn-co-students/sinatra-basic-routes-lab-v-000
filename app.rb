4d0251fc8e576ab773b9f46b17afc693536d0da9
Amanda George <github email address>
2017-06-12 11:59:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Amanda"
+  end
+
+  get "/hometown" do
+    "My hometown is Minneapolis"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Ain't No Man'"
+  end
+
 end
