39fa4a05b538a0a9920688291f3c3671f3572078
Sean Quillen <sean.r.quillen@gmail.com>
2017-06-16 15:02:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Slim Shady."
+  end
+
+  get '/hometown' do
+    "My hometown is my hometown."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is ____"
+  end
 end
