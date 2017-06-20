07cf3cd8da68b1a6ba8346c49767f3cc3a8636e8
Samson Yuwono <samson.yuwono@gmail.com>
2017-06-20 21:29:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get ('/name') do
+    "My name is Samson"
+  end
+
+  get ('/hometown') do
+    "My hometown is Boston"
+  end
+
+  get ('/favorite-song') do
+    "My favorite song is Hotline Bling"
+  end
+
 end
