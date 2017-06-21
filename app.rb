e3b19d1f828f32e05e40d894086097200d198e8c
jordan surgick <jordansurgick@aol.com>
2017-06-21 13:41:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Jordan"
+  end
+
+  get '/hometown' do
+    "My hometown is Camden, NJ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is nothing at this current point.lol"
+  end
 end
