c096b0d469a709ee66ef5bb68118e99459d2f65f
Luisa Scavo <luisascavo@gmail.com>
2017-06-21 15:28:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Luisa!"
+  end
+
+  get '/hometown' do
+    "My hometown is Detroit!"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Rene and Georgette Magritte with Their Dog After the War!"
+  end
 end
