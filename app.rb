fd7874cd034fd1cd2a8a53821ba39b3cc791efea
Lia HUANG <lisa.yc.huang@gmail.com>
2017-06-22 18:08:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Lisa"
+  end
+
+  get '/hometown' do
+  "My hometown is Tainan, Taiwan"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is anything by John Mayer!"
+  end
+
 end
