d563a0120146197a896c607e92da726c056153e5
Scott Jurkowski <scottjurkowski@Crash-Cart.local>
2017-06-24 14:17:49 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is what"
+  end
+
+  get '/hometown' do
+    "My hometown is who"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is scratch"
+  end
 end
