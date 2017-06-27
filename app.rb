5a3f5036defa48b982cdecfa9b7f76ec6b83d7f6
Nathan Campbell <nathancampbell@outlook.com>
2017-06-27 00:57:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Nathan"
+  end
+
+  get '/hometown' do
+    "My hometown is Dayton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bittersweet Symphony"
+  end
+
+
 end
