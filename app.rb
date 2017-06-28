8e6bbb1a3dee0dcea9903d0d57c4536b10cda187
Lara Matyushenko <larabrightside@gmail.com>
2017-06-28 17:31:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Lara"
+  end
+
+  get '/hometown' do
+    "My hometown is Moscow"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Last Nite'"
+  end
 end
