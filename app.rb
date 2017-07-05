cdd5b0eb6c3548ba13795f8be60066ce8410c19e
denisedearman <dearmandenise@gmail.com>
2017-07-05 18:39:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Denise"
+  end
+  get '/hometown' do
+    "My hometown is Longwood"
+  end
+  get '/favorite-song' do
+    "My favorite song is Into the Mystic"
+  end
 end
