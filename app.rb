9bc5349100d344ad3e27d9125863f3d77cf99562
Noah Summers <summers.noah@gmail.com>
2017-07-07 09:26:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Noah Summers"
+  end
+
+  get '/hometown' do
+    "My hometown is the US east coast generally."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Connect' by Foxy Shazam."
+  end
+
 end
