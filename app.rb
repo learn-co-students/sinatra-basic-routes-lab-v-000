05a97674f05a96f17243ab4d6953e91c5e0c846c
charlie lee <ctlee9096@gmail.com>
2017-07-08 16:30:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Charlie Lee"
+  end
+
+  get '/hometown' do
+    "My hometown is Houston, TX"
+  end
+
+  get '/favorite-song' do
+   "My favorite song is Great is Thy Faithfulness"
+  end
 end
