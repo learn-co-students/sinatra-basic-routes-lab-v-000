33ec85108ea34a3d10ef836fd1101bcfb7cad210
Andrew Bunys <awbunys@gmail.com>
2017-07-09 21:33:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+     "My name is Andrew"
+  end
+
+  get '/hometown' do
+    "My hometown is L'Orignal"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Vergiss Uns Nicht"
+  end
+
 end
