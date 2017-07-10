455942fd7e1054371a35deda9fd8bf8b229312d4
hyoyou <heatherhyou@gmail.com>
2017-07-10 13:12:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Heather"
+  end
+
+  get '/hometown' do
+    "My hometown is Norwood, NJ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Happy by Pharrell Williams"
+  end
 end
