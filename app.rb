20daa19001ef0475145839816ab42f06e94a227b
Dustin Anderson <dandersonsoftware@outlook.com>
2017-07-10 01:04:03 +0000

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Dustin"
+  end
+
+  get '/hometown' do
+    "My hometown is Rockford"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Dreaming Tree"
+  end
+
 end
