b2261f8db2f0286a9b06f4bda93891d4b3f56402
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-07-10 07:50:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kaisa"
+  end
+
+  get '/hometown' do
+    "My hometown is London"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hello"
+  end
 end
