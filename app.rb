3d14afc023a98c933e0e8aab886f214a19b1c833
Michael Mirzakandov <mikemirzak@aol.com>
2017-07-12 21:01:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Mike!"
+  end
+
+  get '/hometown' do
+    "My hometown is Miami, FL."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Cold by Post Malone."
+  end
 end
