659a497d326029c3c6485f6129692b9a6347e228
Mel Chang <mel510@users.noreply.github.com>
2017-07-12 22:53:29 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Mel"
+  end
+
+  get '/hometown' do
+    "My hometown is Oakland"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Fly in the Windscreen"
+  end
+
 end
