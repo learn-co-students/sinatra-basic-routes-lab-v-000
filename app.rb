efe52f50118520148ba747062e39411725ab2384
emhopkins <epoletto@lstreetc.com>
2017-07-13 23:02:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do 
+    "My name is Elena"
+  end
+  
+  get '/hometown' do 
+    "My hometown is Aurora, IL"
+  end
+
+  get '/favorite-song' do 
+    "My favorite song is The Schmuel Song"
+  end
+
 end
