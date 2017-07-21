3d75a7c6932a24e934d1fcd67f0c61ff1dc52314
Leonardo Guzman <lguzman@me.com>
2017-07-21 18:44:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		"Hello World!"
+	end
+
+	get '/name' do
+		"My name is Leo"
+	end
+
+	get '/hometown' do
+		"My hometown is Chicago"
+	end
+
+	get '/favorite-song' do
+		"My favorite song is Thuggish Ruggish Bone"
+	end
 end
