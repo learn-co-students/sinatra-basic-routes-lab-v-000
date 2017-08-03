1105fceb158902f35e526670d14410114581d471
Paul Snider <pbsnider@gmail.com>
2017-08-03 18:28:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Paul Snider"
+  end
+
+  get '/hometown' do
+    "My hometown is Saratoga, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is My Way by Frank Sinatra"
+  end
+
 end
