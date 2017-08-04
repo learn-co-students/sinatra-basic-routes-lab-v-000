971e6ff1a3fc48f76c7d40065589b661b2e8db45
Catherine Cordova <ccordova41@gmail.com>
2017-08-04 20:35:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!!!"
+  end
+
+  get '/name' do
+    status 200
+    "My name is __"
+  end
+
+  get '/hometown' do
+    status 200
+    "My hometown is __"
+  end
+
+  get '/favorite-song' do
+    status 200
+    "My favorite song is __"
+  end 
 end
