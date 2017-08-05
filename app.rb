be966319c35316ccca9d43aa1d266c996fef0787
Cynthia Rittenbach <crittenbach@gmail.com>
2017-08-05 18:25:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,30 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    'Hello World!'
+  end
+
+  get '/name' do
+    @name = 'My name is Cynthia'
+ 
+#  erb :'name/models.html.erb'
+  end
+  
+  get '/hometown' do
+    @hometown = 'My hometown is Howell'
+ 
+#  erb erb :'hometown/models.html.erb'
+  end
+  
+  get '/favorite-song' do
+    @favorite_song = 'My favorite song is song'
+ 
+#  erb erb :'favorite_song/models.html.erb'
+  end
+  
+
+  
 end
