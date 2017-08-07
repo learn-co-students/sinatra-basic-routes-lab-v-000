f8aa4b01b03a8c8d8060ff7f3e30e784fb442c7f
Sarah Cyr <sarahcyrdesign@gmail.com>
2017-08-07 23:14:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Sarah"
+  end
+ get '/hometown' do
+   "My hometown is Wallingford, CT"
+ end
+
+ get '/favorite-song' do
+   "My favorite song is 'Alone' by Heart"
+ end
+
+
 end
