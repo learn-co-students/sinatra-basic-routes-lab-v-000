2b8527269bdfa1e11a26838033475b7e960598d0
Soumya Veer <veer.soumya@gmail.com>
2017-08-21 00:05:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Soumya"
+  end
+
+  get '/hometown' do
+    "My hometown is San Mateo"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hello by Adele"
+  end
 end
