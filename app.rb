5bc48727e34930cd0716e5faeb212b0039a1e355
Tyler <tbuchhei@alumni.nd.edu>
2017-08-28 15:28:22 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Tyler"
+  end
+
+  get '/hometown' do
+    "My hometown is Liberty Township"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Here I Dreamt I Was An Architect'"
+  end
 end
