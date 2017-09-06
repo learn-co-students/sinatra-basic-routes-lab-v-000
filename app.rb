3057194d85c25c623433435ccef5c145e97ebffe
Erica Plofsky <ericaplofsky@gmail.com>
2017-09-06 19:38:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Erica."
+  end
+
+get '/hometown' do
+  "My hometown is Livingston, NJ."
+end
+
+get '/favorite-song' do
+  "My favorite song is too many to count."
+end
+
 end
