46ca4f3446f77343bdd37c69b3c30bdd07ca662e
Derek  Pelt <derekpelt@gmail.com>
2017-09-08 02:01:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Derek"
+  end
+
+  get '/hometown' do
+    "My hometown is Fonthill"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Binge&Grab"
+  end
+
 end
