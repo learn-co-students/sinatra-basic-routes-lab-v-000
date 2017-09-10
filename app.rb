8902e655881c5c1f46d56e6d6e5d7a75a54fbda4
Robert Peckham <peckham.rob@gmail.com>
2017-09-10 14:21:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Rob"
+  end
+
+  get '/hometown'do
+    "My hometown is Hamlin, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Kick in the Door"
+  end
 end
