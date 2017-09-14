2afb3c0747035efa678df2f5d9b23fc26558af2c
Jim Stricker <jmstricker93@gmail.com>
2017-09-14 14:50:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+
+  end
+
+  get '/name' do
+    "My name is Jim."
+  end
+
+  get '/hometown' do
+    "My hometown is Harwood, MD"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Livin' On A Prayer"
+  end
+
 end
