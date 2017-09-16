284e68a4b8f3c95ade4043522de6c1942195d865
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2017-09-16 16:19:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Rachel"
+  end
+  #
+  get '/hometown' do
+    "My hometown is Brooklyn"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Idk "
+  end
+
 end
