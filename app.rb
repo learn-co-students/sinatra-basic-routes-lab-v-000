fd6faa02caddb51367d5d90971ed3a5f0627f70c
Archana Burra <burra.archana@gmail.com>
2017-09-29 14:30:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Archana"
+  end
+
+  get '/hometown' do
+    "My hometown is Vizag"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Mask Off' by Future!"
+  end
 end
