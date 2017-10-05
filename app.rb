fd2b1c5441b45c8fe633277cbc55c4627ad651da
ryanwevans <ryanwevans@icloud.com>
2017-10-04 21:17:03 -0700

    added methods/routes to app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Ryan"
+  end
+
+  get '/hometown' do
+    "My hometown is New York City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Freedom by Pharrell"
+  end
+
 end
