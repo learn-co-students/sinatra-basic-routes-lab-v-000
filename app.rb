3eeb5b47f5300a6b5d9f7766351607fb3262e39c
ndalcin <dalcin.nicole@gmail.com>
2017-10-09 09:57:03 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Nicole"
+  end
+  get '/hometown' do
+    "My hometown is Baltimore, MD"
+  end
+  get '/favorite-song' do
+    "My favorite song is LGFUAD by Motion City Soundtrack"
+  end
 end
