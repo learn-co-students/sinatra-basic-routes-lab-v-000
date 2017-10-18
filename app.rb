9fee9f4575e80d9bc33446e885fe333bd0f0f86b
Janusz Szubert <theinvalidnonce@gmail.com>
2017-10-18 18:53:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Janusz."
+  end
+
+  get '/hometown' do
+    "My hometown is NYC."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Wu-Tang Clan - C.R.E.A.M."
+  end
+
 end
