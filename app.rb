f9dac96a3a8d5677bf2e144dcf574675ee6825bc
Robin Pahlman <robin.pahlman@gmail.com>
2017-10-23 15:08:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Robin"
+    end
+
+    get '/hometown' do
+        "My hometown is Brooklyn, NY"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Dum Surfer"
+    end
 end
