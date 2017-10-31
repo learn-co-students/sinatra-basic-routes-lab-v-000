1dba774859c6d7ed366b0deeb66fdeebfa24a4bf
Emily Morgan <emorgan05@gmail.com>
2017-10-31 14:23:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Emily"
+  end
+
+  get '/hometown' do
+    "My hometown is Cary, NC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Take Your Time by Sam Hunt"
+  end
 end
