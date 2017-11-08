cb8bc547e0b1a25e358faa2d03a1201edc105598
kylek <27875726+kylekinnear@users.noreply.github.com>
2017-11-08 10:11:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kyle"
+  end
+
+  get '/hometown' do
+    "My hometown is Greenwich"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is (I have no idea)"
+  end
 end
