5eb5f2967ff8cdf8c653c33dd97d40ebe784841e
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-10 19:19:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,19 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
+    get '/' do
+      "Hello World"
+    end
 
     get '/name' do
-      "My name is #{name}"
-      resp.status = 200
+      "My name is "
     end
 
     get '/hometown' do
-      "My hometown is #{hometown}"
-      resp.status = 200
+      "My hometown is "
+
     end
 
     get '/favorite-song' do
-      "My favorite song is  #{favorite_song}"
-      resp.status = 200
+      "My favorite song is  "
+
     end
+
+end
