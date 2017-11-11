788f3f163c6e8683eabc12447362fbda7f24b1b1
Vicky Lau <vlau.nyc@gmail.com>
2017-11-11 00:45:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get ('/name') {"My name is Vicky."}
+
+  get ('/hometown') {"My hometown is Brooklyn."}
+
+  get ('/favorite-song') {"My favorite song is Happy."}
+
 end
