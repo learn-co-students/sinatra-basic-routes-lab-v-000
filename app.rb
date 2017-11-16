74a497ec311916ea8a701537e672872e9480fa14
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2017-11-16 16:34:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jon."
+  end
+
+  get '/hometown' do
+    "My hometown is Sharon, MA."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Smooth Criminal."
+  end
+
 end
