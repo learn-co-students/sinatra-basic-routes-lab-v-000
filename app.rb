de4a1627ffc44a7fc31169e8e9e8edda3970e527
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-17 17:37:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Koi!"
+  end
+
+  get '/hometown' do
+    "My hometown is Johnson City, TN."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Strawberry Fields Forever."
+  end
 end
