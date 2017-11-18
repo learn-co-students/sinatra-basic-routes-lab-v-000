ec9e49e42683ee9e3f605a42c797a3952485951b
Veronica Miller <vmcmiller@gmail.com>
2017-11-18 18:26:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Veronica."
+  end
+
+  get '/hometown' do
+    "My hometown is Champaign, IL."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Don't Stop Me Now'."
+  end
+
 end
