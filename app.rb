2f135b6bab8f25f117bdb7a0b458925c3c816817
Jessica Bensch <jessica.bensch11@gmail.com>
2017-11-21 15:49:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jessica."
+  end
+
+  get '/hometown' do
+    "My hometown is Toledo"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bicycle."
+  end
 end
