8f4fcef9f9f9afc67325de516b55fec244c30e03
Guesly  Germain <germain1211@outlook.com>
2017-11-22 16:20:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Gues!"
+  end
+
+  get '/hometown' do
+    "My hometown is Haiti"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Boasting by lacrae"
+  end
 end
