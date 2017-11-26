2e1d3fff72dcc25da10a48561e16d7705eeda314
Augusto Granitto <augustogranitto@gmail.com>
2017-11-26 18:09:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Augusto"
+  end
+
+  get '/hometown' do
+  "My hometown is Weston"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is 'Let it Be'"
+  end
+
 end
