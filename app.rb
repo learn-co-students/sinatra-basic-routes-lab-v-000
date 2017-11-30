9b00d10894d64d85b845c7db935285b6b88c5129
Naomi Dennis <njdennis94@gmail.com>
2017-11-30 01:45:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+end
+get '/name' do
+"My name is Naomi."
+end
+get '/hometown' do
+"My hometown is Spring Valley, NY."
+end
+get '/favorite-song' do
+"My favorite song is Delilah By Florence & the Machines."
+end
 end
