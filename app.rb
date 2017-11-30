5bc918a6c9b35416d803cb17f0e96a884c3234a7
Robin Doirin <robindoirin@gmail.com>
2017-11-30 22:47:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Joker!"
+  end
+
+  get '/hometown' do
+    "My hometown is Gotham"
+  end
+
+  get '/favorite-song'do
+    "My favorite song is Baby Blue by Action Bronson "
+  end
 end
