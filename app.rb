d4952e4e1dc31590790784dca6a920e3208082ff
Ben Blackman <bblkmn5@gmail.com>
2017-11-30 21:47:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+#or get('/name') {"My name is Ben"}    
+    get '/name' do
+        "My name is Ben"        
+    end
+    
+#or get('/hometown') {"My hometown is the streets"}
+    get '/hometown' do
+        "My hometown is the streets"
+    end
+    
+# or get('/favorite-song') {"My favorite song is Macarena"}
+    get '/favorite-song' do
+        "My favorite song is Macarena"
+    end
 end
