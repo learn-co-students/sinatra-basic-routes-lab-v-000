e9ac75a39c1648457ed8b3691b1db8f9bd47c769
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-12-06 22:44:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Joey"
+  end
+
+  get '/hometown' do
+    "My hometown is dumb"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is nil"
+  end
+
 end
