cb70b77ea7f2bf92398f54763196def16e0c5d96
Olena Ageyeva <eageeva@hotmail.com>
2017-12-10 20:11:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+  "My name is Olena"
+  end
+  get '/hometown' do
+  "My hometown is Bedford"
+  end
+  get '/favorite-song' do
+  "My favorite song is Christmas"
+  end
 end
