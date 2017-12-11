b2f8ba72d5da1fbdc2426bc8f8facbbfd8d17056
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2017-12-11 12:52:27 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Dana O'Neil"
+  end
+
+  get '/hometown' do
+    "My hometown is Mount Pleasant"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Rest of Our Life - Tim McGraw and Faith Hill"
+  end
 end
