cf1e4ad89244c002c25f1c5035d9076f6d22beef
Susan Fowler <infopro94@gmail.com>
2017-12-16 23:37:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Susan"
+  end
+
+  get '/hometown' do
+    "My hometown is the world"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Spotify"
+  end
+  
 end
