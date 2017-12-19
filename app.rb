2abe998d58f6a43c0574c772fffb7e5cecae96fa
Nour Kodeih <nour.kodeih@gmail.com>
2017-12-19 20:05:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Nour"
+  end
+
+  get '/hometown' do
+    "My hometown is Austn, TX"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is nonexistent."
+  end
 end
