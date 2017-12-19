5a65e3a87453b432fcc1c9dd8363fe0a28e768a5
Travis Petersen <travis.e.petersen@gmail.com>
2017-12-19 01:58:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Travis"
+  end
+  
+  get '/hometown' do
+    "My hometown is Dallas, TX"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Impossible Germany, by Wilco"
+  end
+  
+  
+  
 end
