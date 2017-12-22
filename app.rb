ebb672462baba0c839f86a63fd3386825e9e5a00
Anastasia  Barkett <anastasia.barkett@gmail.com>
2017-12-22 18:05:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is ____"
+  end
+
+  get '/hometown' do
+    "My hometown is _____"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is ___"
+  end
 end
