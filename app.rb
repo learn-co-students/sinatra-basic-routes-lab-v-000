28fc712d954c4ee9620ad2971d70b14a90ed54f4
Chris Boujoukos <github email address>
2018-01-03 16:52:25 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Chris Boujoukos"
+  end
+
+  get '/hometown' do
+    "My hometown is Pittsburgh"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Congratulations'"
+  end
 end
