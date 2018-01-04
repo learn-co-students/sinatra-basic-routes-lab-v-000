1c0f85ef5dbd7e7b60662def9fba2f58d30b6f64
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-04 16:32:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Jered"
+  end
+
+  get "/hometown" do
+    "My hometown is Fort Myers"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is something"
+  end
 end
