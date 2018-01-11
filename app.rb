12cdde055fe9582697060871a93637c53dc3d63b
Richard Chang <rchang9999@yahoo.com>
2018-01-11 19:49:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Richard"
+  end
+
+  get '/hometown' do
+    "My hometown is Nowhere"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Poreotics"
+  end
 end
