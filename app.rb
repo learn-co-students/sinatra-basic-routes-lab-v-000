d662dd395ef2267131cf626555e573a1dd5cdb84
Chris Gullickson <cgullickson5@gmail.com>
2018-01-11 01:09:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Chris"
+  end
+
+  get '/hometown' do
+    "My hometown is Evanston"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is C.R.E.A.M."
+  end
+
 end
