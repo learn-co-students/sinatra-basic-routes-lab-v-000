19468c093d44dc747f860a9cb6bdff54645a13f8
Brittany Morris <brittmmendez@gmail.com>
2018-01-19 18:17:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Brittany"
+  end
+
+  get '/hometown' do
+    "My hometown is Pittsburgh"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Love Story"
+  end
+
+
 end
