a6b97ddb007e1c06a72ea0c88d5ce278fe4c4e4c
Brad Hambrice <hambrice@uab.edu>
2018-01-23 20:55:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Brad"
+  end
+  get '/hometown' do
+    "My hometown is Phenix City"
+  end
+  get '/favorite-song' do
+    "My favorite song is 'Run Away With Me'"
+  end
 end
