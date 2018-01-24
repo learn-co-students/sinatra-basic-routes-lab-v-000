67dd1c7b10f65f61ea77c0e611cfa4a0eec5e4aa
Parker Catalano <catalano.work@gmail.com>
2018-01-24 18:54:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Parker Catalano"
+  end
+
+  get "/hometown" do
+    "My hometown is Carmel"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Numb"
+  end
+
 end
