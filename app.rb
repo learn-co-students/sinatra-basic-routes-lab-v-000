dfbb8bc46705da7ca0c4860d11f1bd2d92e4bd54
PJ Frias <peejfrias@gmail.com>
2018-01-31 15:37:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "SINATRA BASIC ROUTES LAB C'MON!!!!"
+  end
+
+  get '/name' do
+    "My name is PEEEEEEEEEEJ"
+  end
+
+  get '/hometown' do
+    "My hometown is Bel Air"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is the one with the music"
+  end
+
 end
