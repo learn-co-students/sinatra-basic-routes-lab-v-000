0ef75c75da41615e6924143b9e90d7528284b450
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-02-06 01:02:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Szilvia"
+  end
+
+  get "/hometown" do
+    "My hometown is Budapest"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Devils & Dust"
+  end
 end
