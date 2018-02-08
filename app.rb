e3a6814ce64a76ff82ecf27dd0fb9d0bffeddbd3
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-08 21:41:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Homepage"
+  end
+
+  get '/name' do
+    "My name is Maurice"
+  end
+
+
+  get '/hometown' do
+    "My hometown is Theed"
+  end
+
+
+  get '/favorite-song' do
+    "My favorite song is I Wish it Would Rain"
+  end
+
 end
