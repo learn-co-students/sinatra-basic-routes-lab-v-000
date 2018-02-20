8e673e328115e4f7c85b1cc9dd67327d4d39111b
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-20 19:41:18 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Adjoa."
+  end
+
+  get '/hometown' do
+    "My hometown is on the other side of the sunset."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Pink Matter."
+  end
 end
