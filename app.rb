4011e775a832a30b23184a19d6c7caf5ea8a1f9a
Anthony Harvey <anthonygharvey@gmail.com>
2018-02-21 20:41:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  resp = Rack::Response.new
+
+  get '/name' do
+    resp.status = 200
+    resp.write "My name is Anthony Harvey"
+  end
+
+  get '/hometown' do
+    resp.status = 200
+    resp.write "My hometown is Jacksonville, NC"
+  end
+
+  get '/favorite-song' do
+    resp.status = 200
+    resp.write "My favorite song is 'Do I Do' by Stevie Wonder"
+  end
+  
 end
