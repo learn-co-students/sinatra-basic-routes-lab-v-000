9a8fcab8c06be2b65b30c4ee5b25b7ea579a612a
camneu37 <neuner.camille@gmail.com>
2018-02-24 20:08:58 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Camille"
+  end
+
+  get '/hometown' do
+    "My hometown is Pensacola, FL"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'While My Guitar Gently Weeps'"
+  end
+
 end
