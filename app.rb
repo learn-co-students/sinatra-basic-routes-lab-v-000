c548bae3b69b14ed3420d580f9fbf9648d2d811e
kostanzhoglo <jeffreywithers@gmail.com>
2018-02-25 07:20:32 -0500

    Add /name, /hometown, /favorite-song routes to controller

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jeffrey."
+  end
+
+  get '/hometown' do
+    "My hometown is Merrick."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Afterlife."
+  end
+
 end
