85c581c848353ecfac83c577a32e7fd0ae03ca7b
jljardon <github email address>
2018-02-25 20:37:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   get '/' do
+      'Hello!'
+   end
+
+   get '/name' do
+      'My name is Jose L. Jardon'
+   end
+
+   get '/hometown' do
+      "My hometown is ... hmm it's actually kind of hard to explain."
+   end
+
+   get '/favorite-song' do
+      "My favorite song is \"No vacancy\""
+   end
 end
