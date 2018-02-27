0dce661a4b13a20ce6f48000e64e363d1f183a2a
EC2 Default User <ec2-user@ip-172-31-14-103.us-east-2.compute.internal>
2018-02-27 03:36:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get "/name" do
+       "My name is Chrissy" 
+    end
+    
+    get "/hometown" do
+        "My hometown is Corpus Christi"
+    end
+    
+    get "/favorite-song" do
+        "My favorite song is a song"
+    end
 end
