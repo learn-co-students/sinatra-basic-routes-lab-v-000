6a0f2152604997a2947450f2b613f7d575e9ce01
Connor Gallopo <connor.gallopo@me.com>
2018-03-11 20:57:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Connor"
+  end
+  get '/hometown' do
+    "My hometown is Pembroke"
+  end
+  get '/favorite-song' do
+    "My favorite song is I Write Sins Not Tragedies"
+  end
+
 end
