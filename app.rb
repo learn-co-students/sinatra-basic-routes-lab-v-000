f299ac1f3b67d00417319a843261bd9761af7be7
Chris Henry <cdhenry@gmail.com>
2018-03-14 13:57:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Zim"
+  end
+
+  get "/hometown" do
+    "My hometown is Irk"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'When You're Big' by Michael Tavera"
+  end
 end
