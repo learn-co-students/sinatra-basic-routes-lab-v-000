1d9b9d07e65387e5470bd85b177424e6e3e10ab7
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-15 15:35:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+
+    get '/name' do
+    "My name is Aubrey"
+  end
+
+  get '/hometown' do
+    "My hometown is Cleveland"
+  end
+
+  get '/favorite-song'
+    "My favorite song is Hotline-Bling"
+  end
+
 end
