b022888225e90368ffe6a7c1c4bc3c3734d74490
John O'Connor <oconnojb@gmail.com>
2018-03-21 16:17:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is John"
+  end
+
+  get '/hometown' do
+    "My hometown is Loudonville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hungarian Rhapsody No. 2"
+  end
+
 end
