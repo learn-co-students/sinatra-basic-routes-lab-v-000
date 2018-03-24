d965b5d81e4867393bf98ff5961588e3a3b265e4
Carol Greene <carolgreene3@gmail.com>
2018-03-24 20:45:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Carol"
+  end
+
+  get '/hometown' do
+    "My hometown is Naperville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Sit Next to Me"
+  end
+
+
 end
