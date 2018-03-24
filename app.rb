20b65f22ee1a7064bacbbfeff24ee785ead6e98b
Erin Walker <erinb0317@hotmail.com>
2018-03-24 12:40:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Erin"
+  end
+
+  get '/hometown' do
+    "My hometown is Earth"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bicycle by Queen"
+  end 
 end
