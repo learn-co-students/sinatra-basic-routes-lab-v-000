5c263cd0903e62332c6bd867ce1ad91c1153115a
pi-track <patrick.eells@gmail.com>
2018-03-25 09:57:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Pat"
+  end
+
+  get '/hometown' do
+    "My hometown is Philly"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is We didn't start the fire"
+  end
+
 end
