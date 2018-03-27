8a48116fc0244f84bbf31563d77c3ff00f39641e
Jul Jen <juliajen.prog@gmail.com>
2018-03-27 13:59:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jul!"
+  end
+
+  get '/hometown' do
+    "My hometown is currently Jersey City!"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is No Scrubs by TLC"
+  end
+
 end
