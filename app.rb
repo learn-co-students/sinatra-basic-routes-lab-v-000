db103bafbe44f7040efeed5aa2648a804ca4b1a6
Frank McCallion <fkmccallion@gmail.com>
2018-04-09 12:13:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Keith"
+  end
+
+  get "/hometown" do
+    "My hometown is Ojai"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Fire Woman"
+  end
+
 end
