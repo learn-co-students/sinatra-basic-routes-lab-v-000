635f11fb6db60a93e9dbcb1c2ee7cad9e5a4cf39
Cassandra Dickson <cassandradickson7@gmail.com>
2018-04-21 03:31:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Cassandra."
+  end
+
+  get '/hometown' do
+    "My hometown is Seattle."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is something."
+  end
 end
