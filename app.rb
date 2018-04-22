187eecf345c49edf28e06aabe2100ba5ca33a7c3
Bryan Hooper <bryan@lindsayfarm.net>
2018-04-22 18:04:16 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Bryan Hooper."
+  end
+
+  get '/hometown' do
+    "My hometown is Houston, Texas."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Magdalene by Guy Clark."
+  end
+  
 end
