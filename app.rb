5bbd1be1e7174f001f934246248a9f10e67dc5fc
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-23 19:33:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Jon!"
+  end
+
+  get '/hometown' do
+    "My hometown is Golden, CO"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Rick Flair' by Migos!"
+  end
+
 end
