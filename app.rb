f966ea89d5110bdb09bc7a4881cd117db24b2904
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-03 16:00:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Petya!"
+  end
+
+  get '/hometown' do
+    "My hometown is Sofia."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Galabo'"
+  end
+
 end
