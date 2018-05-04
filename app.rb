7e1daedfb2a7afdfc86ac9c163d9997d6dd8ccd6
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-04 14:15:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Emily.\n"
+  end
+
+  get '/hometown' do
+    "My hometown is Shenyang."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is everything by Avicii/Ed Sheeran."
+  end
 end
