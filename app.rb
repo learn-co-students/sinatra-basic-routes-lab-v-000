139f2dd2870d9f720be9fa21c230601b3067cf43
David Markel <davidgmarkel@gmail.com>
2018-05-10 18:39:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is David."
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Liberation' by Outkast."
+  end
+
+
+
 end
