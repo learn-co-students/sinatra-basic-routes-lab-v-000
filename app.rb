0d8bad7560a2a0f0b6457fb400a21b6f97b1257e
Kriti Rai <kriti.rai@gmail.com>
2018-05-13 22:16:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Kriti."
+  end
+
+  get '/hometown' do
+    "My hometown is Kathmandu."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'In the aeroplane over the sea' by Neutral Milk Hotel."
+  end
 end
