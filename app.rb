0fc48d51bb03136a36fd080a6485884c6e9a759f
Emily Jirles <emily.jirles@gmail.com>
2018-05-24 22:13:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Emily"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Lore City, OH"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Ends of the Earth by Lord Huron"
+  end 
+  
 end
