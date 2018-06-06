786cdf4e3800d6a06172501fbe1e0fe0c1aadf6c
ludaires <ludaires@gmail.com>
2018-06-05 22:28:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Ludmilla"
+    end
+
+    get '/hometown' do 
+        "My hometown is Goiânia "
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Cry for the Moon, by Epica"
+    end
+
 end
