48a7721978298e5670b859b886ebbe54172643a2
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-06-10 19:29:51 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Sonia"
+  end
+
+  get '/hometown' do
+    "My hometown is Cicero"
+  end
+
+  get '/favorite-song' do
+      "My favorite song is Quimbara"
+  end
+
 end
