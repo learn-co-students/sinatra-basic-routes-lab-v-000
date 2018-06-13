eaef272b7fd9b7e86d35dd8a6d02e5c78362fadb
Brett Weisberg <weisberg.brett@gmail.com>
2018-06-13 22:46:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Brett"
+  end
+
+  get '/hometown' do
+    "My hometown is in Conn."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Let it be"
+  end
+
 end
