2671c1031f15f56fd3f36fca333aa83612d186a4
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-13 14:38:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-
+    "My name is Temitope Elodimuor"
   end
+
+  get '/hometown' do
+    "My hometown is Ontario"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Breakaway"
+  end
+
 end
