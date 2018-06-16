7971a5f23360b3ee105a9e1e681f846679842fbc
Karl Amoguis <ktamoguis@gmail.com>
2018-06-16 07:46:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+      "Hello, World!"
+  end
+
+  get '/name' do
+      "My name is Karl."
+  end
+
+  get '/hometown' do
+      "My hometown is Manila, Philippines."
+  end
+
+  get '/favorite-song' do
+      "My favorite song is Rozes."
+  end
+
 end
