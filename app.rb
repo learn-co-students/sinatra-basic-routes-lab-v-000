7bd4a69fe2627c89bd5e22782e6d05f3372af12f
Alex Zhang <alexgzhang92@gmail.com>
2018-06-21 01:24:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "This is the homepage"
+  end
+  
+  get '/name' do
+    "My name is Alex"
+  end
+  
+  get '/hometown' do
+    "My hometown is Southern Pines"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is \"Fake Happy\""
+  end
+  
 end
