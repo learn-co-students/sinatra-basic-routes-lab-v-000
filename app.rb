0cd25d72e85db4a6ad85892b506e43b1e73e1ca8
Pamela Frieze <pfrieze@gmail.com>
2018-06-23 21:50:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Pam"
+  end
+
+  get '/hometown' do
+    "My hometown is Chappaqua."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Closer."
+  end
+
 end
