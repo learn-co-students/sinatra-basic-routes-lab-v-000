8e46430ecf6cbb3c8aedcb9a397ea6b7d78f565f
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-26 16:20:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Jeremiah"
+  end
+
+  get '/hometown' do
+    "My hometown is Wheaton"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Sober'"
+  end
 end
