838c14f0878d03f4a25b98e048cec7af288fd242
Dani Schuhman <dani8439@aol.com>
2018-07-10 16:55:42 +0000

    second time through lab

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is "
+    "My name is __"
   end
 
   get '/hometown' do
-    "My hometown is "
+    "My hometown is __"
   end
 
   get '/favorite-song' do
-    "My favorite song is "
+    "My favorite song is __"
   end
 
 end
