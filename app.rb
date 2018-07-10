7d3658014e53d19a085b0bcdacd649ea2c91cbd6
Hillevi Eklöw <hillevieklow@gmail.com>
2018-07-10 21:40:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Hillevi"
+  end
+
+  get '/hometown' do
+    "My hometown is Varobacka"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Say my name' by Odesza"
+  end
 end
