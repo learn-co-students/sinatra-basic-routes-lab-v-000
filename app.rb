85d5bb32a3b4e4dd9cee1b874afe84cecc2747f6
Kayla Reopelle <kreopelle@gmail.com>
2018-07-11 14:27:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Kayla."
+  end
+
+  get '/hometown' do
+    "My hometown is Graham, WA."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Mr. Blue Sky' by Electric Light Orchestra."
+  end
+
 end
