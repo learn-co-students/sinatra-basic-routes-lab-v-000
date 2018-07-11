7f2cb97004b519fb2d6b5022766faa4b83e38151
theresamorelli <theresa.l.morelli@gmail.com>
2018-07-11 14:02:34 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Theresa"
+  end
+
+  get '/hometown' do
+    "My hometown is Grand Junction, Colorado"
+  end
+  
+  get '/favorite-song' do
+    "Absolutely impossible to pick just one song but... from 2018, My favorite song is Money by Caroline Rose"
+  end
 end
