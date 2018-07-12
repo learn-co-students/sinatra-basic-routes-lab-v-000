197522b6fb607c4ff97a4ea50ffe517c90eff3b9
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-12 15:38:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kayla"
+  end
+
+  get '/hometown' do
+    "My hometown is Brownsville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is This is Me"
+  end
+
 end
