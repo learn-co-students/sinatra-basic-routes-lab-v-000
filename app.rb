a4351eddbfea9a480fbcc6db2891d53601c08e10
iamdavidmichaelmoore <hey@iamdavidmoore.com>
2018-07-14 12:39:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is David."
+    end
+
+    get '/hometown' do
+        "My hometown is Fort Lauderdale."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Sleep."
+    end
+
+
 end
