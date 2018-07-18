cdfff5cb9a39068c256e16484200f360cd006f38
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-18 22:43:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Babs."
+  end
+
+  get '/hometown' do
+    "My hometown is San Mateo, CA."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'When You Wish Upon A Star'."
+  end
+
 end
