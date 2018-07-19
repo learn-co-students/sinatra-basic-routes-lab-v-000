22921fae7d67d6d3e8b973909bb7b3a0216e63dd
Christopher P. Dundon <chris.dundon@gmail.com>
2018-07-19 17:49:03 -0400

    lab complete

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	get '/' do
+		"Hello, World!"
+	end
+
+	get '/name' do
+		"My name is Chris Dundon!"
+	end
+
+	get '/hometown' do
+		"My hometown is Great Neck, NY."
+	end
+
+	get '/favorite-song' do
+		"My favorite song is (Ghost) Riders in the Sky as performed by Johnny Cash."
+	end
 end
+
