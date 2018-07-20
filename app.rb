664a1dea0781ea8db9a4aea5adbe6d0c5b7255c5
Kayvan Vakili <kvakili813@gmail.com>
2018-07-20 21:27:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World! Test"
+  end
+
+  get '/name' do
+    "My name is Sunny!"
+  end
+
+  get '/hometown' do
+    'My hometown is Pakistan'
+  end
+
+  get '/favorite-song' do
+    "My favorite song is none"
+  end
+
 end
