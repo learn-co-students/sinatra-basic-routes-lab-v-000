e40829d25623c1101448dceea204c3fe499e41f0
Nancy Jo <enciie@gmail.com>
2018-08-01 00:05:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Nancy Jo"
+  end
+
+  get '/hometown' do
+    "My hometown is Los Angeles"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Twinkle Twinkle Little Star"
+  end
+
 end
