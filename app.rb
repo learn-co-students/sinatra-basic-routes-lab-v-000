57f8d51d7f6a1b1d665c541fbd7239cba6e81fc9
Josh Cesana <joshuacesana@gmail.com>
2018-08-02 00:47:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"  
+  end 
+  
+  get '/name' do 
+    "My name is Josh"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is San Ramon, CA."
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Thriller."
+  end 
+  
 end
