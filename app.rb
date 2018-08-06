9045c7ebd1971249f6d10fcced776178adc0d858
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-06 21:36:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    'Hello, World!'
+  end
+
+  get '/name' do
+    "My name is Jim"
+  end
+
+  get '/hometown' do
+    "My hometown is Brick"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Trash Stains"
+  end
+
+   
 end
