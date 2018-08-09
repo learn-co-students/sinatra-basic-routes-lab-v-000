9c22ef9557f1469dc4d8cd799a0e1d7e1a1987d1
Andrew Nikonchuk <anikonch@gmail.com>
2018-08-09 18:54:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Andrew Nikonchuk"
+  end
+
+  get '/hometown' do
+    "My hometown is Methuen, MA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Rolling in the Deep'"
+  end
+
 end
