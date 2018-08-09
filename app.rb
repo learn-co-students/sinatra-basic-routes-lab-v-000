49e24acc573a79da117602746b74012dcffe15fd
Joana Palero <joana.palero24@gmail.com>
2018-08-09 01:07:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
-
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Joana"
+end
+
+get '/hometown' do
+  "My hometown is Saipan"
+end
+
+get '/favorite-song' do
+  "My favorite song is Doo Wop"
+end
+
 end
