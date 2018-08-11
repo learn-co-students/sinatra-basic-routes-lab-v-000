4c2354b1a154a13c2eac613f10b27d71401a94d3
Ariel Zhen <zhenpingping@live.com>
2018-08-11 20:31:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Ariel!"
+  end
+
+  get '/hometown' do
+    "My hometown is Hong Kong!"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Hello."
+  end
+  
 end
