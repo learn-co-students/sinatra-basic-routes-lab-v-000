91a89d263bde9771fb65cea2924dcfa256e4a483
Cory Tveten <corytveten@gmail.com>
2018-09-02 12:50:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Cory Tveten"
+  end
+
+  get '/hometown' do
+    "My hometown is Redlands, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'What a Fool Believes'"
+  end
 end
