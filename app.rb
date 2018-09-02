749c92bcc66a1cec833fb5b2202584c03e829d4e
Mark Lawrence <lawrence.ml@gmail.com>
2018-09-02 16:30:19 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Blank."
+  end
+
+  get '/hometown' do
+    'My hometown is Blankville.'
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Blank Space."
+  end
 end
