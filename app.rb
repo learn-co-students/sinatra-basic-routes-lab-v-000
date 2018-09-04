92e73ce28967a303adfadbdd0b2d16248be110d7
Brandon Green <brandon@brandonleeproductions.com>
2018-09-04 00:55:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Lynie!"
+  end
+  get '/hometown' do
+    "My hometown is Buffalo!"
+  end
+  get '/favorite-song' do
+    "My favorite song is any by U2!"
+  end
 end
