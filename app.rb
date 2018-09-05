465c1e0337e44f2df98bfa55136ceb25963286ab
Argonity <maurice.argoetti@gmail.com>
2018-09-05 20:08:45 -0300

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Maurice"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The World"
+  end
+
 end
