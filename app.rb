ebe6846ba3fa06e97ff4b9ba0bb3b5c3c436ec36
Mary Jack <maryjackinthebox@gmail.com>
2018-09-05 17:21:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Mary Jack"
+  end
+   get '/hometown' do
+    "My hometown is Denver, Colorado"
+  end
+   get '/favorite-song' do
+    "My favorite song is Running Up that Hill by Kate Bush"
+  end
+  
 end
