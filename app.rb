0e16889032cf5228ab28358ee80b9446f740dc2b
Adam Duff <j.adam.duff@gmail.com>
2018-09-05 10:25:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Adam Duff"
+  end
+
+  get '/hometown' do
+    "My hometown is Charlotte, NC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is My Way by Frank Sinatra"
+  end
+
 end
