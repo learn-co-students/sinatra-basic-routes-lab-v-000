310a005b8131507cbea100971d34e647c5d6acb9
Brenden Thornton <thorntonbrenden@gmail.com>
2018-09-07 14:28:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Brenden."
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Miami, FL."
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is 505."
+  end 
+  
 end
