7ef2327793e1f06fe6ccfda4bfce067b6a0ca1e8
Eric Chrobak <eric@ericchrobak.com>
2018-09-27 16:28:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Your app works"
+  end
+
+  get '/name' do
+    "My name is Eric"
+  end
+
+  get '/hometown' do
+    "My hometown is Lindenhurst"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Fire Fall Down"
+  end
 end
