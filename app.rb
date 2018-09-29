fff00cf7c4924d460ef2a65d54a2f1fa816095c7
Catherine Salazar <catcreates13@gmail.com>
2018-09-29 03:17:01 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Catherine"
+  end
+
+  get '/hometown' do
+    "My hometown is La Filipina"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Real by Plumb"
+  end
+
+
 end
