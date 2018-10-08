2544785800799287622622594ef849c10e0ac9b7
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-08 01:52:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/name" do
+      "My name is Mike"
+    end
+    get "/hometown" do
+      "My hometown is Long Beach"
+    end
+    get "/favorite-song" do
+      "My favorite song is Koyaanisqatsi"
+    end
 end
