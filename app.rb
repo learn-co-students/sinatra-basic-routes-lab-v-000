7ab58caa4397637412310f8e744c10b489f344a9
Will Schultz <will.j.schultz@gmail.com>
2018-10-12 03:31:52 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+     "My name is Will"
+  end
+
+  get '/hometown' do
+     "My hometown is Vernon"
+  end
+
+  get '/favorite-song' do
+     "My favorite song is Sandstorm"
+  end
+
 end
