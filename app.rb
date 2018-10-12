c1f7761fa9b9b7ed321c52c6ac725f937be13a81
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-12 08:58:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end 
+  
+  get '/name' do
+    "My name is Gaby"
+  end 
+  
+  get '/hometown' do
+    "My hometown is San Salvador"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is no idea"
+  end 
+  
+  
 end
