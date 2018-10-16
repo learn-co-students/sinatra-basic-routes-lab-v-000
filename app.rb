ac99ca85b2dae05e2f041f42f03366ca2005f293
valeria campilongo <cvaleriac@hotmail.it>
2018-10-16 19:32:03 +0000

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+  "My name is Valeria"
+end
+
+get '/hometown' do
+    "My hometown is NY"
+  end
+   get '/favorite-song' do
+    "My favorite song is Volare"
+  end
+
 end
