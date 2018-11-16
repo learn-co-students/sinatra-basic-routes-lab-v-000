a2ea8fcaf91abfd917f36dad629ba1a759d0f548
George  Pianka <gpianka1@gmail.com>
2018-11-16 12:32:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get ('/name'){"My name is George."}
+
+get ('/hometown'){"My hometown is Pocantico Hills."}
+
+get ('/favorite-song'){"My favorite song is 'Losing My Religion."}
+
 end
+
+
