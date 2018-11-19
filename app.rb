c6f0fe716ca559a25fca8feb4fa6af935529b1cd
Rima Jean <rima.jean@gmail.com>
2018-11-19 21:35:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Rima!"
+  end
+
+  get '/hometown' do
+    "My hometown is Houston, Texas."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bohemian Rhapsody."
+  end
 end
