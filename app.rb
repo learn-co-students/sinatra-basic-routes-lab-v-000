4c5460b4d777dc4a68d3766533e1fcf73d516f01
Meg Gutshall <meghan.gutshall@gmail.com>
2018-12-13 19:31:56 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Meg."
+  end
+
+  get '/hometown' do
+    "My hometown is North Wales."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Three Little Birds."
+  end
+
 end
