2f64296d8ea33ed30f8a8ea4fcf2379ee3cdba72
Benjamin Jones <bljones36@gmail.com>
2018-12-17 00:57:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Ben"
+  end
+
+  get '/hometown' do
+    "My hometown is Lindenwold"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is In the End"
+  end
 end
