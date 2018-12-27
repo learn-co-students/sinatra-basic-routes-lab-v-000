058b2958462d5f21a03735a4a5c437b29b62688f
anihakutin <4hbrody@gmail.com>
2018-12-26 21:34:19 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Heshie Brody!"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is dependent on my mood... :)"
+  end
 end
