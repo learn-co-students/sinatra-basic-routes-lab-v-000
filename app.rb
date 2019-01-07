a95dd204c1ea76ec9f64a277665bdcac5fa46373
Vince Andrew Pimentel <contact@vincep.me>
2019-01-07 00:45:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Vince."
+  end
+
+  get "/hometown" do
+    "My hometown is San Fernando."
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Shelter."
+  end
 end
