26edb91ff1df4ae9e52161abf859ae36042e01c5
Tom Cho <tc.thomascho@gmail.com>
2019-01-18 23:05:09 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    [200, {"content-type" => "text/html"}, ["Hello, World!"]]
+  end
+
+  get '/name' do
+    [200, {"content-type" => "text/html"}, ["My name is Tom"]]
+  end
+
+  get '/hometown' do
+    [200, {"content-type" => "text/html"}, ["My hometown is Scotch Plains"]]
+  end
+
+  get '/favorite-song' do
+    [200, {"content-type" => "text/html"}, ["My favorite song is Machinehead"]]
+  end
+
 end
