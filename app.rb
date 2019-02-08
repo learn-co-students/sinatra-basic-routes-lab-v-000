ed41622fc7ac075c06fa2682740247c57f78821d
Tyler Sell <tylerselldesign@gmail.com>
2019-02-08 04:35:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    'My name is Tyler Sell'
+  end
+  
+  get '/hometown' do 
+    'My hometown is Clinton, MO.'
+  end
+  
+  get '/favorite-song' do 
+    'My favorite song is "The City Never Sleeps" by Cartel.'
+  end
+  
 end
