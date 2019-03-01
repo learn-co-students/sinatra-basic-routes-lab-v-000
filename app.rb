ef88532391015a9b97af1875c1371df89c218677
Katie Moosman <moosman.katie@gmail.com>
2019-03-01 16:13:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Katie"
+  end
+
+  get '/hometown' do
+    "My hometown is Salt Lake City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is by George Strait"
+  end
+
+
+
 end
