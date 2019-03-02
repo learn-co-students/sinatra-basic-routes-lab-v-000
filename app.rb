a3fdca69d2fc559d58d92e9e55af4a98cba705eb
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-03-02 15:55:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Richard Salchunas"
+  end
+
+  get '/hometown' do
+    "My hometown is Greenland, NH"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'The Old Black Hole'"
+  end
+
 end
