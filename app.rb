254237bf2704563a0c81f32d9c6d030cd8c73251
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-01 21:16:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Mariel"
+  end
+
+  get '/hometown' do
+    "My hometown is Rego Park"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Sunrise"
+  end
+
 end
