559ced38d3d6f16578636329b8880789d9efa0b4
Ryan Brady <ryanbrady369@gmail.com>
2019-04-25 18:47:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get ('/name') {"My name is Ryan"}
+
+  get ('/hometown') {"My hometown is Mahopac"}
+
+  get ('/favorite-song') {"My favorite song is idk"}
 end
