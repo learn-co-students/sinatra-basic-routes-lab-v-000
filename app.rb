1ac5a70c57e7b67f5c480428ab1a566524d618c6
Amil Masic <amil.masic@gmail.com>
2019-06-22 16:11:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Amil."
+  end
+
+  get '/hometown' do
+    "My hometown is Sanski Most."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Africa by Toto."
+  end
 end
