46b45b5d5f168d79bd6411c4fa49cda540452230
jamieberrier <berrier.jamie@gmail.com>
2019-07-01 21:56:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jamie"
+  end
+
+  get '/hometown' do
+    "My hometown is Chandler, AZ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'I Want To Dance With Somebody'"
+  end
+
 end
